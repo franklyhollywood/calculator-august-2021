f1203ab2a869d760fcc1c6fee5122b8761a70dbe
Frank Hollywood <github@srvsync.com>
2021-08-12 17:03:47 -0700

    created calculator - divide not working yet

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 // import functions and grab DOM elements
+import { add, subtract, multiply } from './utils.js';
 
-// initialize state
+const addEl1 = document.getElementById('addValue1');
+const addEl2 = document.getElementById('addValue2');
+const addButton = document.getElementById('addButton');
+const resultEl1 = document.getElementById('totalAdd');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//console.log(addEl1, addEl2, addButton, resultEl);
+addButton.addEventListener('click', () => {
+	const addFirstVal = addEl1.value;
+	const addSecondVal = addEl2.value;
+
+	const sum = add(addFirstVal, addSecondVal);
+
+	resultEl1.textContent = sum;
+});
+//////////////////
+const subtractEl1 = document.getElementById('subtractValue1');
+const subtractEl2 = document.getElementById('subtractValue2');
+const subtractButton = document.getElementById('subtractButton');
+const resultEl2 = document.getElementById('totalSubtract');
+
+//console.log(subtractEl1, subtractEl2, subtractButton, resultEl2);
+subtractButton.addEventListener('click', () => {
+	const subtractFirstVal = subtractEl1.value;
+	const subtractSecondVal = subtractEl2.value;
+
+	const sum = subtract(subtractFirstVal, subtractSecondVal);
+
+	resultEl2.textContent = sum;
+});
+
+//////////////////
+const multiplyEl1 = document.getElementById('timesValue1');
+const multiplyEl2 = document.getElementById('timesValue2');
+const multiplyButton = document.getElementById('timesButton');
+const resultEl3 = document.getElementById('totalTimes');
+
+//console.log(subtractEl1, subtractEl2, subtractButton, resultEl2);
+multiplyButton.addEventListener('click', () => {
+	const multiplyFirstVal = multiplyEl1.value;
+	const multiplySecondVal = multiplyEl2.value;
+
+	const sum = multiply(multiplyFirstVal, multiplySecondVal);
+
+	resultEl3.textContent = sum;
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,132 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { add, subtract, multiply } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+test('add should take in 5 + -5 and return 0 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = 0;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = add(5, -5);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+
+test('add should take in "5" + -7 and return -2 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = -2;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = add('5', -7);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+
+test('add should take in 8 + 2 and return 10 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = 10;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = add(5, 5);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+////
+test('subtract should take in 5 - -5 and return 10 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = 10;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = subtract(5, -5);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+
+test('subtract should take in "5" - -7 and return 12 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = 12;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = subtract('5', -7);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+
+test('subtract should take in 8 - 2 and return 6 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = 6;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = subtract(8, 2);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+
+////
+test('multiply should take in 5 * -5 and return -25 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = -25;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = multiply(5, -5);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+
+test('multiply should take in "5" * -7 and return -35 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = -35;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = multiply('5', -7);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
+});
+
+test('multiply should take in 8 * 2 and return 16 ', (expect) => {
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = 16;
+
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = multiply(8, 2);
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
 });
